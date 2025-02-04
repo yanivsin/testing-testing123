ff8ce74eeb1f3e62335f36884dc61a05db8c948d
Lint Action <lint-action@samuelmeuli.com>
2025-02-04 09:07:58 +0000

    Fix code style issues with Black

--- dags/pythontest.py
+++ dags/pythontest.py
@@ -1,5 +1,4 @@
 import logging
 
-count = [x for x in range(1,20,2) ]
-print( "hi there  " )
-
+count = [x for x in range(1, 20, 2)]
+print("hi there  ")
--- dags/seaside.py
+++ dags/seaside.py
@@ -1,5 +1,5 @@
-test = 'this is a test for a pull_request!'
+test = "this is a test for a pull_request!"
 
 
 for char in test:
-    print('hi   '  )
+    print("hi   ")
