ceb72986be872d5b880837744852b9f8456b6aff
yanivsin <112955131+yanivsin@users.noreply.github.com>
2025-02-04 10:50:44 +0200

    testing (#76)
    * testing
    
    * add main

--- seaside.py
+++ seaside.py
@@ -1,15 +1,5 @@
-import logging
-import pandas
-
 test = 'this is a test for a pull_request!'
+
+
 for char in test:
-    print('hi')
-
-dfghjddasdsupdadasadasdadasd
-
-dsfsaaaaa
-
-sdfaaa
-aaa
-#gblgingaaa
-dadasaaa
+    print('hi   '  )
